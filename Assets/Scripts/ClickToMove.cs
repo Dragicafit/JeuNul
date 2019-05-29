cc605ad75a3bb66fc8508dc1678af8282cac5fbf
Dragicafit <dherbert98@gmail.com>
2019-05-29 16:54:08 +0200

    charactereController

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -3,49 +3,38 @@
 public class ClickToMove : MonoBehaviour
 {
 
-    public float Speed = 5f;
-    public float JumpHeight = 2f;
-    public float GroundDistance = 0.2f;
+    public float speed = 5f;
+    public float jumpHeight = 8f;
+    public float gravity = 20f;
     //public float DashDistance = 5f;
 
-    private Rigidbody body;
-    private Collider col;
-    //private LesBeauxPiedsDuCube lbpdc;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController characterController;
 
     void Start()
     {
-        body = GetComponent<Rigidbody>();
-        col = GetComponent<Collider>();
-        /*
-        lbpdc = GetComponentInChildren<LesBeauxPiedsDuCube>();
-        if (lbpdc == null)
-            throw new System.Exception("Tu peux pas marcher sans pieds");
-        */
+        characterController = gameObject.GetComponent<CharacterController>();
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        bool IsGrounded = Physics.Raycast(transform.position, Vector3.down, GroundDistance);
-        //bool IsGrounded = false;// lbpdc.IsGrounded();
-
-        Vector3 moveDirection = Vector3.zero;
-        if (Input.GetButton("Fire1"))
+        if (characterController.isGrounded)
         {
-            moveDirection = new Vector3(Input.mousePosition.x - Screen.width / 2, 0.0f, Input.mousePosition.y - Screen.height / 2).normalized;
-            body.MovePosition(body.position + moveDirection * Speed * Time.fixedDeltaTime);
-        }
+            moveDirection = Vector3.zero;
+            if (Input.GetButton("Fire1"))
+            {
+                moveDirection = new Vector3(Input.mousePosition.x - Screen.width / 2, 0.0f, Input.mousePosition.y - Screen.height / 2).normalized;
+                moveDirection *= speed;
+            }
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpHeight;
+            }
 
-        if (Input.GetButtonDown("Jump") && IsGrounded)
-        {
-            body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
         }
-
-        /*
-        if (Input.GetButtonDown("Dash"))
-        {
-            Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * body.drag + 1)) / -Time.deltaTime)));
-            body.AddForce(dashVelocity, ForceMode.VelocityChange);
-        }
-        */
+        moveDirection.y -= gravity * Time.deltaTime;
+        
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
